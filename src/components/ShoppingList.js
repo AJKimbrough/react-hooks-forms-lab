30bfccecd74ef07e8d71b957ff9fa0fc72c6e342
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-06-16 08:57:41 -0500

    lab work

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,10 +5,21 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [foodSearch, setFoodSearch] = useState("")
+console.log(foodSearch)
+  function handleFoodSearch(event) {
+    setFoodSearch(event.target.value)
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+
+  const itemsSearch = items.filter((item) => {
+    if(foodSearch !== ""){
+      return items.name
+    }
+  })
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -19,11 +30,12 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleFoodSearch} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
+        <li>{foodSearch}</li>
       </ul>
     </div>
   );
